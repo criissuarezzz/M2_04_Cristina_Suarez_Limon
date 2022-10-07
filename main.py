56f7f87eab52a69f50f8519949d98db299244e4c
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-10-07 10:46:38 +0200

    lista tupla

---
+++ main.py
@@ -0,0 +1,6 @@
+pruebas_atl= [200, 100, longitud]
+prueba1= pruebas_atl[1]
+prueba2= pruebas_atl[2]
+prueba3= pruebas_atl[3]
+
+print("Mis pruebas favoritas de atletismo son", prueba1, prueba2, "y", prueba3)
